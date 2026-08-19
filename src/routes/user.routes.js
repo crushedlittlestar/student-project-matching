033ec20b7397018a1336f3b1fc5f0904ec00747a
Jana Ashraf <janaashraf6000@gmail.com>
2026-08-19 16:03:02 +0300

    feat(users): add profile update endpoint

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { getMe } = require("../controllers/user.controller");
+const { getMe, updateMe } = require("../controllers/user.controller");
 const authenticate = require("../middlewares/auth.middleware");
 
 const router = express.Router();
 
 router.get("/users/me", authenticate, getMe);
+router.patch("/users/me", authenticate, updateMe);
 
 module.exports = router;
